1d396cc3f27f9290f21fe0f18fc4b2253fc67ba3
Yusuke Yamamoto <yusuke@samuraism.com>
2018-11-20 19:15:04 +0900

    ほげほげ

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -16,9 +16,15 @@ import static mypackage.HelloWorld.getHello;
 
 
 
-
 public class Main {
     public static void main(String... args) {
-        System.out.println("Hello world!!");
+        String hello = null;
+        hello = getHello();
+        System.out.println(hello);
     }
 }
+
+
+
+
+
