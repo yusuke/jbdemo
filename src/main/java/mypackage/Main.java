d62d36e5c6854c096c63661df8f02be8c0960f70
Yusuke Yamamoto <yusuke@samuraism.com>
2018-11-20 17:39:35 +0900

    Initial commit

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -19,8 +19,6 @@ import static mypackage.HelloWorld.getHello;
 
 public class Main {
     public static void main(String... args) {
-        String hello = null;;
-        hello = getHello();
-        System.out.println(hello);
+        System.out.println("Hello World!");
     }
 }
