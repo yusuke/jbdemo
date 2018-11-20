039f8f2d34b9fb0a98d6c0613bd0c6da6e2e82f4
Yusuke Yamamoto <yusuke@mac.com>
2018-11-20 15:42:51 +0900

    Initial commit

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -4,6 +4,6 @@ import static mypackage.HelloWorld.getHello;
 
 public class Main {
     public static void main(String... args) {
-        System.out.println("Hello W  orld!");
+        System.out.println("Hello W orld!");
     }
 }
