3fea50c49d904f40ccac737d85727b9a97a3091b
Yusuke Yamamoto <yusuke@samuraism.com>
2018-11-20 19:24:13 +0900

    JBDEMO-1 テスト

--- src/main/java/mypackage/Main.java
+++ src/main/java/mypackage/Main.java
@@ -18,7 +18,7 @@ import static mypackage.HelloWorld.getHello;
 
 public class Main {
     public static void main(String... args) {
-        System.out.println("Hello world!!");
+        System.out.println(getHello());
     }
 }
 
